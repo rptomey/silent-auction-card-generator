439c3691cb8e07f2828e88ae9664bbe08fccc280
Ryan Ptomey <rptomey6@gmail.com>
2025-10-31 17:08:34 -0500

    added wrap text method

--- generate_cards.py
+++ generate_cards.py
@@ -14,6 +14,32 @@ with open(CONFIG_FILE, "r") as file:
     CONFIG = json.load(file)
 # ---------------------
 
+# https://dev.to/emiloju/wrap-and-render-multiline-text-on-images-using-pythons-pillow-library-2ppp
+def wrap_text(text, font, max_width, draw):
+    """
+    Wraps text to fit within the max_width.
+    """
+    words = text.split()
+    lines = [] # Holds each line in the text box
+    current_line = [] # Holds the current line under evaluation.
+
+    for word in words:
+        # Check the width of the current line with the new word added
+        test_line = ' '.join(current_line + [word])
+        width = draw.textlength(test_line, font=font)
+        if width <= max_width:
+            current_line.append(word)
+        else:
+            # If the line is too wide, finalize the current line and start a new one
+            lines.append(' '.join(current_line))
+            current_line = [word]
+
+    # Add the last line
+    if current_line:
+        lines.append(' '.join(current_line))
+
+    return lines
+
 def create_card(item_name, starting_bid, auction_url, template_file):
     """Generates a single auction card."""
     
